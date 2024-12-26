7a3d4421d82d9fca9a49829205a5e2575f56593f
mhmdhasbi <mhmdhasbi08@gmail.com>
2024-12-27 01:19:13 +0700

    change default web server to flask

--- app.py
+++ app.py
@@ -295,6 +295,6 @@ def index():
     )
 
 # Ini diperlukan oleh Vercel untuk mengenali WSGI
-# app = app
-if __name__ == '__main__':
-    app.run(debug=True)
+app = app
+# if __name__ == '__main__':
+#     app.run(debug=True)
